9f80d87b794385fdee95248182bf8d076cbae48b
david musembi <dmusembi702@gmail.com>
2026-08-10 12:42:12 +0300

    Set up react project and added components

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,50 @@
+import { Menu } from "lucide-react";
+
+export default function Navbar() {
+  return (
+    <nav className="fixed top-0 w-full z-50 transition-all duration-300 bg-slate-950/20 backdrop-blur-sm border-b border-slate-800">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex justify-between items-center h-14 sm:h-16 md:h-20">
+
+          <div className="flex items-center space-x-3 group cursor-pointer">
+            <div>
+              <img
+                src="/Ufungamano round logo.png"
+                alt="Christian Student Leadership Centre"
+                className="w-8 h-8 sm:w-12 sm:h-12"
+              />
+            </div>
+            <span className="text-lg sm:text-xl md:text-2xl font-medium">
+              <span className="text-white">Christian Student</span>{' '}
+              <span className="text-orange-400">Leadership Centre</span>
+              <span className="text-orange-200 block text-center text-sm sm:text-base italic">Ufungamano House</span>
+            </span>
+          </div>
+
+          <div className="hidden md:flex justify-between gap-8">
+            <a href="#home" className="text-gray-300 hover:text-orange-500 text-sm lg:text-base">
+              Home
+            </a>
+            <a href="#about" className="text-gray-300 hover:text-orange-500 text-sm lg:text-base">
+              About
+            </a>
+            <a href="#programmes" className="text-gray-300 hover:text-orange-500 text-sm lg:text-base">
+              Programmes
+            </a>
+            <a href="#services" className="text-gray-300 hover:text-orange-500 text-sm lg:text-base">
+              Catering Services
+            </a>
+            <a href="#contact" className="text-gray-300 hover:text-orange-500 text-sm lg:text-base">
+              Contact
+            </a>
+          </div>
+
+          <button className="md:hidden">
+            <Menu className="w-5 h-5 sm:w-6 sm:h-6" />
+          </button>
+
+        </div>
+      </div>
+    </nav>
+  );
+}
---
+++ src/components/Pricing.jsx
@@ -0,0 +1,5 @@
+export default function Pricing() {
+    return <nav className="fixed top-0 w-full z-50 transion-all duratiomn-300">
+        
+    </nav>;
+}
